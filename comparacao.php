38ee8079a9c9c56c65f432632d890de5695d4404
Flavio tavares <tavares.pmsg@gmail.com>
2020-05-14 18:47:47 -0300

    Operadores de Comparação

---
+++ comparacao.php
@@ -0,0 +1,24 @@
+<?php
+
+$a =10;
+$b =10;
+
+ if($a == $b){
+    echo "A é igual a B.";
+ }else{
+     echo "A é diferente B.";
+ }
+
+ if($a != $b){
+    echo "A é diferente de B.";
+ }else{
+     echo "A é igual B.";
+ }
+
+ if($a >= $b){
+    echo "A é >= do que B.";
+ }else{
+     echo "A é < do que B.";
+ }
+
+?>
