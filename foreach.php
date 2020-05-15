f0a3d9444537998580bb2f70519b789bba60c49f
Flavio tavares <tavares.pmsg@gmail.com>
2020-05-15 11:41:54 -0300

    ForEach

---
+++ foreach.php
@@ -0,0 +1,17 @@
+<?php
+
+$planetas = [
+                'Mercúrio',
+                'Vênus',
+                'Terra',
+                'Marte',
+                'Júpiter',
+                'Saturno',
+                'Urano',
+                'Netuno'
+        ];
+
+
+foreach($planetas as $planeta){
+    echo "$planeta <br>";
+}
