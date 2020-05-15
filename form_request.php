ba339349a5e4222c1ccff9a636b877dcadf54699
Flavio tavares <tavares.pmsg@gmail.com>
2020-05-15 17:53:39 -0300

    REQUEST, SERVER

---
+++ form_request.php
@@ -0,0 +1,7 @@
+
+
+<form action="request.php?nome=Gilson" method="post">
+
+    <input type="text" name="cor">
+
+</form>
---
+++ request.php
@@ -0,0 +1,3 @@
+<?php
+// A variável global $_REQUEST busca valores dos Métodos GET e POST.
+var_dump($_REQUEST);
